470d61b6a8e866e5983def51178cfc0fe9fa5940
LeticiaCMZ <leticmz@gmail.com>
2021-04-21 17:56:44 -0300

    Letflix Files

---
+++ js/owl/setup.js
@@ -0,0 +1,39 @@
+$(() => {
+
+$('#carousel1').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+
+
+
+$('#carousel2').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+
+});
